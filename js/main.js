a099bca0a5d1763fa1d40a659ff67d9ee89a10a9
Matteojk95 <matteopalmer2095@gmail.com>
2023-06-29 16:15:24 +0200

    finish basic work

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// chiedo all'utente il suo nome, il suo cognome e poi il suo colore preferito
+
+let nome= prompt("inserisci il tuo nome:");
+let surname= prompt("inserisci il tuo cognome:");
+let prefercolor= prompt("inserisci il tuo colore preferito:");
+
+// scrivi sulla console nomecognomecolorepreferito21
+
+let nomecognomecolorepreferito21= nome + surname+ prefercolor+21;
+
+
+console.log (`
+${nomecognomecolorepreferito21}
+`);
